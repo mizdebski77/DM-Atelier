326a69c43608d3969c2b74b8d714c4e31ead2ee5
Marcin Izdebski <mizdebski123@gmail.com>
2023-06-21 20:59:25 +0200

    Added navbar

--- src/common/Navbar/navbar.tsx
+++ src/common/Navbar/navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Links, LinksWrapper, Logo, Wrapper } from './styledNavbar';
+import { Links, LinksWrapper, Logo, LogoLink, Wrapper } from './styledNavbar';
 import logo from '../Images/DM Logo.png'
 
 export const Navbar = () => {
     return (
         <Wrapper>
-            <Logo src={logo} alt='Logo'></Logo>
+            <LogoLink to="/Strona-Główna"><Logo src={logo} alt='Logo'/> </LogoLink>
             <LinksWrapper>
                 <Links to="/Strona-Główna">Strona Główna</Links>
                 <Links to="/Oferta">Oferta</Links>
--- src/common/Navbar/styledNavbar.ts
+++ src/common/Navbar/styledNavbar.ts
@@ -1,28 +1,54 @@
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export const Wrapper = styled.nav`
     width: 100%;
     display: grid;
     grid-template-columns: 1fr 1fr;
     align-items: center;
-    padding: 20px;
+    padding: 20px 64px;
+`;
+
+export const LogoLink = styled(Link)`
+
 `;
 
 export const Logo = styled.img`
-    width: 400px;
+    width: 460px;
 `;
 
 export const LinksWrapper = styled.div`
-    display: flex;
-    justify-content: space-around;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    align-items: center;
+    gap: 64px;
+    margin: 0 40px;
 `;
 
-export const Links = styled(NavLink)`
-    color: ${({theme}) => theme.color.secondColor};
-    text-decoration: none;
+const activeClassName = "active";
 
-    :active {
-        color: red;
+export const Links = styled(NavLink)`
+    color: ${({ theme }) => theme.color.secondColor};
+    text-decoration: none;
+    font-size: 16px;
+    text-align: center;
+    border-radius: 20px;
+    transition: 0.5s;
+    text-underline-offset: 6px;
+
+    &:hover {
+        transform: translateY(-5px);
+        text-decoration: underline;
+    }
+
+    &.${activeClassName} {
+        background: ${({ theme }) => theme.color.secondColor};
+        padding: 10px;
+        color: ${({ theme }) => theme.color.mainColor};
+
+        &:hover {
+            transform: none;
+            text-decoration: none;
+        }
     }
 `;

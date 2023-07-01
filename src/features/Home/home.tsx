611db08d030e33115ac67493a8c463dd11a6dc86
Marcin Izdebski <mizdebski123@gmail.com>
2023-07-01 18:51:29 +0200

    Added settings to slider

--- src/features/Home/home.tsx
+++ src/features/Home/home.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Title, Wrapper } from './styledHome';
-
+import { CustomSlider, Header, Image, Paragraph, Title, Wrapper } from './styledHome';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ex1 from '../../common/Images/Example Images/pngwing.com (1).png'
 export const Home = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+    };
     return (
         <Wrapper>
-            <Title>D'M Atelier z miłości do barmaństwa</Title>
+            <Header>
+                <Title>D'M Atelier <Paragraph>z miłości do barmaństwa</Paragraph> </Title>
+                <CustomSlider {...settings}>
+                    <Image src={ex1} />
+                    <Image src={ex1} />
+                    <Image src={ex1} />\
+                </CustomSlider>
+            </Header>
+
         </Wrapper>
     );
 };
--- src/features/Home/styledHome.ts
+++ src/features/Home/styledHome.ts
@@ -1,14 +1,36 @@
 import styled from "styled-components";
+import Slider from "react-slick";
 
-export const Wrapper = styled.main`
+export const Wrapper = styled.div`
+
+`;
+
+export const Header = styled.header`
     width: 100%;
     display: grid;
     grid-template-columns: 1fr 1fr;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const Title = styled.h1`
-    font-size: 60px;
+    font-size: 48px;
     font-style: italic;
     font-weight: 500;
+    margin: 0;
+    text-align: center;
 `;
+
+export const Paragraph = styled.p`
+    margin: 0;
+`;
+
+export const Image = styled.img`
+
+`;
+
+export const CustomSlider = styled(Slider)`
+    max-width: 500px;
+    margin: 0 auto;
+`;  
 

e319dabeaec8fe20fba9d42d46559c070f63728a
Marcin Izdebski <mizdebski123@gmail.com>
2023-07-24 18:43:00 +0200

    Added animation for photo

--- src/core/animations.ts
+++ src/core/animations.ts
@@ -13,3 +13,19 @@ export const sectionAnimation = {
         },
     },
 };
+
+export const photosAnimation = {
+    hidden: {
+        opacity: 0,
+        x: 100,
+    },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            type: "spring",
+            duration: 0.6,
+        },
+    },
+
+}
